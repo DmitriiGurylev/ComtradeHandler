f5cecfa9c33234e21e018307faea8eaca2723f1b
Wonderful-guy <gurylev.dmitrii@mail.ru>
2022-04-27 21:40:40 +0300

    unused transformPrimaryValuesToSecondary method removed [2]

--- ComtradeHandler.java
+++ ComtradeHandler.java
@@ -15,23 +15,23 @@ public class ComtradeHandler {
 	/**
 	 * args:
 	 * 1. transform all analog values from primary to secondary and write it to the same path:
-	 * comtrade #path_to_comtrade# transformall #primary_value# #secondary_value#
+	 * comtrade #path_to_comtrade# transform #primary_value# #secondary_value#
 	 * 2. transform all analog values from primary to secondary and write it to a specific path:
-	 * comtrade #path_to_comtrade# transformall #primary_value# #secondary_value# write #path_to_write_to#
+	 * comtrade #path_to_comtrade# transform #primary_value# #secondary_value# write #path_to_write_to#
 	 * 3. transform specific analog values from primary to secondary:
 	 * comtrade #path_to_comtrade# transform #array of analog value to transform# #primary_value# #secondary_value#
 	 * comtrade #path_to_comtrade# transform #array of analog value to transform# #primary_value# #secondary_value# write #path_to_write_to#
-	 * **/
+	 **/
 
 	public static void main(String[] args) {
 		ActionsWithComtrade actionsWithComtrade = new ActionsWithComtrade();
-		int i=0;
+		int i = 0;
 		if (args[i].equals("comtrade")) {
 			Path pathToFile = Path.of(args[i + 1]);
 
 			Comtrade comtrade = new Comtrade();
 			try {
-				comtrade.setCfg(Files.readString(Paths.get(pathToFile+".cfg"), StandardCharsets.US_ASCII));
+				comtrade.setCfg(Files.readString(Paths.get(pathToFile + ".cfg"), StandardCharsets.US_ASCII));
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -41,44 +41,24 @@ public class ComtradeHandler {
 				e.printStackTrace();
 			}
 			i = i + 2;
-			if (args[i].equals("transformall")) {
-				Comtrade comtradeUpd = actionsWithComtrade.transformPrimaryValuesToSecondary(
-						comtrade,
-						null,
-						Float.parseFloat(args[i + 1]),
-						Float.parseFloat(args[i + 2]));
-				i = i + 3;
-				Path pathOfUpdatedComtrades = i < args.length && args[i].equals("write") ?
-						Path.of(args[i + 1] + "/UpdatedComtrades") :
-						Path.of(pathToFile.getParent() + "/UpdatedComtrades");
-				try {
-					Files.createDirectories(pathOfUpdatedComtrades);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				String fileName = pathOfUpdatedComtrades + "/" + pathToFile.getFileName();
-				try {
-					Files.write(Paths.get(fileName + ".cfg"), comtradeUpd.getCfg().getBytes());
-					Files.write(Paths.get(fileName + ".dat"), String.valueOf(comtradeUpd.getDat()).getBytes());
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			} else if (args[i].equals("transform")) {
+			Comtrade comtradeUpd;
+			if (args[i].equals("transform")) {
 				int countOfSignalsToTransform = (int) IntStream.range(i + 1, args.length)
-						.takeWhile(j -> !args[j].equals("write")).count()-2;
-				if (countOfSignalsToTransform <= 0) {
+						.takeWhile(j -> !args[j].equals("write")).count() - 2;
+				if (countOfSignalsToTransform < 0) {
 					return;
 				}
-				Set<Integer> arrayOfAnalogSignals = IntStream.range(i + 1, args.length)
-						.mapToObj(j -> Integer.parseInt(args[j]))
-						.collect(Collectors.toSet());
-
-				Comtrade comtradeUpd = actionsWithComtrade.transformPrimaryValuesToSecondary(
+				Set<Integer> arrayOfAnalogSignals = (countOfSignalsToTransform == 0) ? null :
+						IntStream.range(i + 1, args.length)
+								.mapToObj(j -> Integer.parseInt(args[j]))
+								.collect(Collectors.toSet());
+				comtradeUpd = actionsWithComtrade.transformPrimaryValuesToSecondary(
 						comtrade,
 						arrayOfAnalogSignals,
 						Float.parseFloat(args[i + 1]),
 						Float.parseFloat(args[i + 2]));
-				i = i + 3;
+				i = i + 2 + countOfSignalsToTransform;
+
 				Path pathOfUpdatedComtrades = i < args.length && args[i].equals("write") ?
 						Path.of(args[i + 1] + "/UpdatedComtrades") :
 						Path.of(pathToFile.getParent() + "/UpdatedComtrades");
